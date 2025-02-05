b466050d59ab94cd91fa94425aa271e70585f39d
Ben Golding <bgolding.42lausanne@gmail.com>
2025-02-05 16:25:03 +0100

    push_swap tester: checker added
    Now uses checker binary to check the push_swap actually sorts the given
    input correctly.
    
    Any test-cases where the checker does not explicitly print out "OK" are
    appended to the KO_FILE that can be inspected after execution.
    
    An error report is displayed if any such cases arise.

--- push_swap/my_tester.sh
+++ push_swap/my_tester.sh
@@ -11,9 +11,16 @@ declare -i numberoftests=$2
 declare -i testnumber=1
 declare -i total=0
 declare -i already_sorted=0
+declare -i ko_count=0
 ELEMS=$3
-RAW="raw_results"
-RESULTS="results.txt"
+TMPDIR="tester_tmp_files"
+RAW="$TMPDIR/raw_results"
+RESULTS="$TMPDIR/results.txt"
+MOVES="$TMPDIR/moves.txt"
+KO_FILE="KO.txt"
+
+mkdir $TMPDIR
+touch $RAW $RESULTS $MOVES
 
 echo Running $numberoftests tests for $ELEMS elements.
 
@@ -21,8 +28,15 @@ while [[ $? -eq 0 && $testnumber -le $numberoftests ]]; do
 	echo -en "\r$testnumber/$numberoftests"
 	declare -i count
 	ARG=$(shuf -i 0-10000 -n $ELEMS | tr '\n' ' ')
-	count=$(./$EXECUTABLE $ARG | wc -l)
-	if [ $count -ne 0 ]; then
+	count=$(./$EXECUTABLE $ARG | tee $MOVES | wc -l)
+	CHECKER_SAYS=$(./checker $ARG <$MOVES)
+	if [ ! $CHECKER_SAYS = "OK" ]; then
+		if [ $ko_count -eq 0 ]; then
+			echo "" > $KO_FILE
+		fi
+		ko_count=$(($ko_count + 1))
+		echo -e "[ Case $ko_count ]\n$ARG\n" >> $KO_FILE
+	elif [ $count -ne 0 ]; then
 		echo $count >> $RAW
 		total=$(($total + $count))
 	else
@@ -34,8 +48,13 @@ done
 echo ""
 
 sort -n $RAW | cat > $RESULTS
-rm $RAW
 
 echo -e "Best:\\t" $(head -n 1 $RESULTS)
 echo -e "Avg:\\t" $( expr $total / $(expr $numberoftests - $already_sorted))
 echo -e "Worst:\\t" $(tail -n 1 $RESULTS)
+
+rm -rf $TMPDIR
+
+if [ $ko_count -gt 0 ]; then
+	echo -e "KO:\\t $ko_count/$numberoftests\\tSee $KO_FILE for details"
+fi
