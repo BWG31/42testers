c48b0b2a05ca764ea93cb4f32db5235339756467
Ben Golding <bgolding.42lausanne@gmail.com>
2025-06-02 14:04:25 +0200

    updated run with limits

--- memorylimiter/run_with_limits.sh
+++ memorylimiter/run_with_limits.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-if [ "$#" -lt 1 ]; then
-	echo "Usage: $0 <memory limit>"
+if [ "$#" -lt 2 ]; then
+	echo "Usage: $0 <executable> <memory limit>"
 	exit 1
 fi
 
-memory_limit=$1
+memory_limit=$2
+executable=$1
 shift
 
 ulimit -v "$memory_limit"
 
-./miniRT_bonus scenes/bonus/test/red_wall.rt
+$executable
+
